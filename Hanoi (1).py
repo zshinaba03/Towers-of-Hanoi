e83e18560f95286574570e2e6d2eac873a0b2e33
zshinaba03 <zshinaba@gmail.com>
2024-08-15 16:28:49 -0400

    Add files via upload

---
+++ Hanoi (1).py
@@ -0,0 +1,31 @@
+import timeit
+from Stack import Stack
+
+def Hanoi_rec(n, s, a, d):
+  print(n, s, a, d)
+  # TODO replace pass with your base and recursive cases.
+  if n == 0:
+    d.push(s.pop())
+  else:
+    Hanoi_rec(n-1, s, d, a)
+    d.push(s.pop())
+    Hanoi_rec(n-1, a, s, d)
+
+
+  print(n, s, a, d)
+  print()
+
+def Hanoi(n):
+  source = Stack()
+  dest = Stack()
+  aux = Stack()
+  i = n-1
+  while i >= 0:
+    source.push(i)
+    i = i - 1
+  Hanoi_rec(n-1, source, aux, dest)
+
+if __name__ == "__main__":
+  n=3
+  runtime = timeit.timeit("Hanoi(n)", setup="from __main__ import Hanoi,n", number=1)
+  print("computed Hanoi(" + str(n) + ") in " + str(runtime) + " seconds.")
